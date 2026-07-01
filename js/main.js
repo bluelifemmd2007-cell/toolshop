c6e8709feb3e6cfec1b577ddaa17a78f974784f2
Mohammad Mahdi Mohammadi <bluelifemmd2007@gmail.com>
2026-07-01 14:08:40 -0700

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("menuBtn")?.addEventListener("click", () => {
-  document.getElementById("navLinks")?.classList.toggle("open");
-});
-
-document.querySelectorAll(".flash").forEach((el) => {
-  setTimeout(() => el.classList.add("hide"), 4000);
-});
